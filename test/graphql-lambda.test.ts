663a263376a80ca8c784956744103fc2d0270e8b
MichaelLinares <mglinares.2419@gmail.com>
2022-06-29 08:10:18 -0400

    Initial commit

---
+++ test/graphql-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GraphqlLambda from '../lib/graphql-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/graphql-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GraphqlLambda.GraphqlLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
